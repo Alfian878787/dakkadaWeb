fd6e9bf4d2a4964ada0f63d49ba2f56cce6198e0
Killian Thompson <juniorkillian@gmail.com>
2016-11-22 15:24:07 +0100

    Express Config 50%

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 var express = require('express'); //Imports express into my application
 var http = require('http');
+var swig = require('swig');
 
 var  app = express(); //I call on express` createServer() called app
 var port = process.env.NODE_ENV || 8280;
 var colors = require('colors');
-var swig = require('swig');
+// var swig = require('swig');
+
 
 // view engine config
 app.engine('html', swig.renderFile);
 
+
 app.set('view engine', 'html');
+app.set('views', __dirname + '/public/views');
+
 
 //this is our route
 app.get('/', function(req, res){
-    res.send('This is my first Express Application');
+    res.render('index');
 });
 
 var server = http.createServer(app);
